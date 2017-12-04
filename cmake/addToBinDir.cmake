ce5627191cbc8744334b79c8857db61701900281
Daniel Bonrath <bonne@clockwork-origins.de>
2017-12-04 22:26:43 +0100

    #6 added Qt as dependency

--- cmake/addToBinDir.cmake
+++ cmake/addToBinDir.cmake
@@ -3,6 +3,32 @@ IF(WIN32)
 	foreach(filename ${files})
 		execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different ${filename} ${CMAKE_BINARY_DIR}/bin)
 	endforeach()
+
+	file(GLOB_RECURSE files "$ENV{Qt5_DIR}/bin/icudt*.dll")
+	foreach(filename ${files})
+		get_filename_component(filename ${filename} NAME)
+		configure_file($ENV{Qt5_DIR}/bin/${filename} ${CMAKE_BINARY_DIR}/bin/${filename} COPYONLY)
+	endforeach()
+
+	file(GLOB_RECURSE files "$ENV{Qt5_DIR}/bin/icuin*.dll")
+	foreach(filename ${files})
+		get_filename_component(filename ${filename} NAME)
+		configure_file($ENV{Qt5_DIR}/bin/${filename} ${CMAKE_BINARY_DIR}/bin/${filename} COPYONLY)
+	endforeach()
+
+	file(GLOB_RECURSE files "$ENV{Qt5_DIR}/bin/icuuc*.dll")
+	foreach(filename ${files})
+		get_filename_component(filename ${filename} NAME)
+		configure_file($ENV{Qt5_DIR}/bin/${filename} ${CMAKE_BINARY_DIR}/bin/${filename} COPYONLY)
+	endforeach()
+
+	configure_file($ENV{Qt5_DIR}/bin/libEGL.dll ${CMAKE_BINARY_DIR}/bin/libEGL.dll COPYONLY)
+	configure_file($ENV{Qt5_DIR}/bin/libGLESv2.dll ${CMAKE_BINARY_DIR}/bin/libGLESv2.dll COPYONLY)
+	configure_file($ENV{Qt5_DIR}/plugins/platforms/qwindows.dll ${CMAKE_BINARY_DIR}/bin/plugins/platforms/qwindows.dll COPYONLY)
+
+	configure_file(${CMAKE_SOURCE_DIR}/config/qt.conf ${CMAKE_BINARY_DIR}/bin/qt.conf COPYONLY)
+
+	configure_file($ENV{Qt5_DIR}/bin/Qt5Core.dll ${CMAKE_BINARY_DIR}/bin/Qt5Core.dll COPYONLY)
 ENDIF(WIN32)
 IF(UNIX)
 	file(GLOB_RECURSE files "${GOTHICSCRIPTANALYZER_DEP_DIR}/*.so*")
